d97256c9f83932171e1652cbb21449c6650da143
PapuAngelo <angelofranco2911@gmail.com>
2023-11-30 19:05:10 -0600

    Update Act6Triangulo

--- Act6Triangulo.cpp
+++ Act6Triangulo.cpp
@@ -1,32 +1,32 @@
 #include <iostream>
-#include "Triangulo.h"
-#include "Punto.h"
+#include "Triangle.h"
+#include "Point.h"
 using namespace std;
 
-void displayTriangleArray(Triangulo arrTriangles[], int size) {
+void displayTriangleArray(Triangle arrTriangles[], int size) {
     for (int index = 0; index < size; index++) {
         cout << index << ". " << arrTriangles[index].str() << endl;
     }
 }
 
-void readTriangleArray(Triangulo arrTriangles[], int size) {
+void readTriangleArray(Triangle arrTriangles[], int size) {
     double x1, y1, x2, y2, x3, y3;
 
     for (int index = 0; index < size; index++) {
         cout << "Enter the 3 x, y coordinates for the vertices of triangle " << index << ":";
         cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
 
-        Punto point1{ x1, y1 };
-        Punto point2{ x2, y2 };
-        Punto point3{ x3, y3 };
+        Point point1{ x1, y1 };
+        Point point2{ x2, y2 };
+        Point point3{ x3, y3 };
 
-        arrTriangles[index].setVertice1(point1);
-        arrTriangles[index].setVertice2(point2);
-        arrTriangles[index].setVertice3(point3);
+        arrTriangles[index].setVertex1(point1);
+        arrTriangles[index].setVertex2(point2);
+        arrTriangles[index].setVertex3(point3);
     }
 }
 
-void calculatePerimeterArea(Triangulo arrTriangles[], int size) {
+void calculatePerimeterArea(Triangle arrTriangles[], int size) {
     for (int index = 0; index < size; index++) {
         cout << "Triangle " << index << ".\nPerimeter = " <<
             arrTriangles[index].perimeter() <<
@@ -37,13 +37,13 @@ void calculatePerimeterArea(Triangulo arrTriangles[], int size) {
 
 int main()
 {
-    Punto pointA{ -5, -5 }, pointB{ 1,3 }, pointC{ 4, -6 };
-    Punto pointD{ -2, -3 }, pointE{ 5,-4 }, pointF{ -1, 3 };
-    Punto pointG{ -2, -3 }, pointH{ 5,-4 }, pointI{ -1, 3 };
-    Triangulo triangle1{ pointD,pointE,pointF }, triangle2{ pointA,pointB,pointC }, triangle3{ pointG,pointH,pointI };
+    Point pointA{ -5, -5 }, pointB{ 1,3 }, pointC{ 4, -6 };
+    Point pointD{ -2, -3 }, pointE{ 5,-4 }, pointF{ -1, 3 };
+    Point pointG{ -2, -3 }, pointH{ 5,-4 }, pointI{ -1, 3 };
+    Triangle triangle1{ pointD,pointE,pointF }, triangle2{ pointA,pointB,pointC }, triangle3{ pointG,pointH,pointI };
 
-    const int arraySize = 3; // Adjust the array size accordingly
-    Triangulo arrTriangles[arraySize];
+    const int arraySize = 3;
+    Triangle arrTriangles[arraySize];
 
     displayTriangleArray(arrTriangles, arraySize);
     readTriangleArray(arrTriangles, arraySize);
